b9cfe741cc13762dfd86de7c5462a997343b6a17
Michel Massaad <michel_01ft@hotmail.com>
2026-05-01 10:44:21 -0300

    Inicializazion del proyecto Angular

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Tp_Sala_de_juegos');
+}
